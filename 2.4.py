b947d25257414256e6a01a19dd587a89c78b17ba
VadimPlotnikov <VadimPlotnikov@users.noreply.github.com>
2017-06-14 21:44:52 +0300

    Add files via upload

---
+++ 2.4.py
@@ -0,0 +1,15 @@
+import subprocess
+import os
+from os import listdir
+
+list_files = (listdir ('./Sourdce/'))
+
+for ff in list_files:
+    rr = './Sourdce/'+ff #создание пути к исходному файлу
+    tt = './Result/'+ff #путь для результата
+    jj = 'convert.exe '+ rr+ ' -resize 200 '+tt # создание аргумента для subprocess.run
+    nn = subprocess.run(jj)
+
+
+
+
